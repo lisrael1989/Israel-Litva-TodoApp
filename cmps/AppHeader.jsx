f1ebe79856de2f30a47a73592e5ed77fa1c300ca
israel <lisrael1989@gmail.com>
2024-03-22 17:09:13 +0200

    adding user login,logout and signup

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,6 +1,33 @@
 const { Link, NavLink } = ReactRouterDOM;
+const { useState } = React;
+const { useSelector, useDispatch } = ReactRedux;
+const { useNavigate } = ReactRouter;
+
+import { UserMsg } from "./UserMsg.jsx";
+import { LoginSignup } from "./LoginSignup.jsx";
+import { userService } from "../services/user.service.js";
+import { showErrorMsg, showSuccessMsg } from "../services/event-bus.service.js";
+import { logout } from "../store/actions/user.actions.js";
 
 export function AppHeader() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((storeState) => storeState.loggedInUser);
+  function onLogout() {
+    logout()
+      .then(() => {
+        showSuccessMsg("logout successfully");
+      })
+      .catch((err) => {
+        showErrorMsg("OOPs try again");
+      });
+  }
+
+  function onSetUser(user) {
+    setUser(user);
+    navigate("/");
+  }
+
   return (
     <header className="app-header full main-layout">
       <section className="header-container">
@@ -9,12 +36,9 @@ export function AppHeader() {
           <NavLink to="/">Home</NavLink>
           <NavLink to="/about">About</NavLink>
           <NavLink to="/todo">Todo</NavLink>
-          {/* <a onClick={onToggleCart} href="#">
-            🛒 Cart
-          </a> */}
         </nav>
       </section>
-      {/* {user ? (
+      {user ? (
         <section>
           <span to={`/user/${user._id}`}>
             Hello {user.fullname} <span>${user.score.toLocaleString()}</span>
@@ -23,11 +47,10 @@ export function AppHeader() {
         </section>
       ) : (
         <section>
-         
           <LoginSignup />
         </section>
       )}
-      <UserMsg /> */}
+      <UserMsg />
     </header>
   );
 }
