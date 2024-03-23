225410bbd320716673d47953bc038a077065d80b
israel <lisrael1989@gmail.com>
2024-03-23 10:49:02 +0200

    adding footer

--- cmps/AppFooter.jsx
+++ cmps/AppFooter.jsx
@@ -5,12 +5,12 @@ const { useSelector } = ReactRedux;
 
 export function AppFooter() {
   // const dispatch = useDispatch();
-  // const todosLength = useSelector((storeState) => storeState.todos.length);
+  const todosLength = useSelector((storeState) => storeState.todos.length);
 
   return (
     <footer className="app-footer">
-      {/* <h5>Currently {todosLength} cars in the shop</h5> */}
-      <h1>test</h1>
+      {/* <h5>Currently {todosLength} todos in the shop</h5> */}
+      <h4 className="copy-rights">copyrights israel litvak</h4>
       <UserMsg />
     </footer>
   );
